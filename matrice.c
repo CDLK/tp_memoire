796c66c7b98e395b2442e14741751ae6afa1fac1
cdlk <popoto38697@gmail.com>
2019-11-14 11:41:33 +0100

    Matric lineaire valide

--- matrice.c
+++ matrice.c
@@ -2,29 +2,42 @@
 #include <stdlib.h>
 
 matrice allouer_matrice(int l, int c) {
-    matrice m = { 0, 0, NULL };
+  if(l > 0 && c > 0){
+    double **donnes = malloc(sizeof(double*)*l);
+    for (int i = 0; i < l; i++) {
+      donnes[i] = malloc(sizeof(double)*c);
+    }
+    matrice m = { l, c, donnes };
     return m;
+  } else {
+    matrice m = { -1, -1, NULL };
+    return m;
+  }
 }
 
 void liberer_matrice(matrice m) {
+  for (int i = 0; i < m.l; i++) {
+    free(m.donnees[i]);
+  }
+  free(m.donnees);
 }
 
 int est_matrice_invalide(matrice m) {
-    int resultat = 0;
-    return resultat;
+  return ((m.l == -1 && m.c == -1) ? 1 : 0);
 }
 
 double *acces_matrice(matrice m, int i, int j) {
-    double *resultat = NULL;
-    return resultat;
+  if(est_matrice_invalide(m)==0 && i < m.l && j < m.c){
+    return &m.donnees[i][j];
+  } else {
+    return NULL;
+  }
 }
 
 int nb_lignes_matrice(matrice m) {
-    int resultat = 0;
-    return resultat;
+  return m.l;
 }
 
 int nb_colonnes_matrice(matrice m) {
-    int resultat = 0;
-    return resultat;
+  return m.c;
 }
--- matrice_lineaire.c
+++ matrice_lineaire.c
@@ -2,29 +2,36 @@
 #include <stdlib.h>
 
 matrice allouer_matrice(int l, int c) {
-    matrice m = { 0, 0, NULL };
+  if(l > 0 && c > 0){
+    double *donnes = malloc(sizeof(double*)*l*c);
+    matrice m = { l, c, donnes };
     return m;
+  } else {
+    matrice m = { -1, -1, NULL };
+    return m;
+  }
 }
 
 void liberer_matrice(matrice m) {
+  free(m.donnees);
 }
 
 int est_matrice_invalide(matrice m) {
-    int resultat = 0;
-    return resultat;
+  return ((m.l == -1 && m.c == -1) ? 1 : 0);
 }
 
 double *acces_matrice(matrice m, int i, int j) {
-    double *resultat = NULL;
-    return resultat;
+  if(est_matrice_invalide(m)==0 && i < m.l && j < m.c){
+    return &m.donnees[i*m.c+j];
+  } else {
+    return NULL;
+  }
 }
 
-int nb_lignes_matrice(matrice m) {
-    int resultat = 0;
-    return resultat;
+int nb_lignes_matrice(matrice m){
+  return m.l;
 }
 
-int nb_colonnes_matrice(matrice m) {
-    int resultat = 0;
-    return resultat;
+int nb_colonnes_matrice(matrice m){
+  return m.c;
 }
